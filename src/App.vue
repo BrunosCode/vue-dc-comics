5a9b8aead6b87958cb3afc88b053ed19fa6ca6a3
brunEdo <edobruno.m@gmail.com>
2021-09-29 19:58:24 +0200

    footerBottom without Style

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <Main/>
     <Banner/>
     <FooterTop/>
+    <FooterBottom/>
   </div>
 </template>
 
@@ -12,6 +13,7 @@ import Header from './components/Header.vue'
 import Main from './components/Main.vue'
 import Banner from './components/Banner.vue'
 import FooterTop from './components/FooterTop.vue'
+import FooterBottom from './components/FooterBottom.vue'
 
 export default {
   name: 'App',
@@ -19,7 +21,8 @@ export default {
     Header,
     Main,
     Banner,
-    FooterTop
+    FooterTop,
+    FooterBottom,
   }
 }
 </script>
---
+++ src/components/FooterBottom.vue
@@ -0,0 +1,68 @@
+<template>
+<div class="c-footerBottom">
+    <div class="l-container l-row l-row--spacebetween">
+        <a class="c-btn c-ctaBtn c-footerBottom__btn">Sign-up Now!</a>
+
+        <div class="l-row">
+            <p>Folow now!</p>
+            <ul class="c-footerBottom__iconsLinks h-listReset l-row">
+                <li v-for="(link, i) in iconLinks" :key="i">
+                    <a :href="link.url"
+                    class="c-footerBottom__link c-link">
+                        <img :src="require(`@/assets/images/${link.img}`)" :alt="link.text"
+                        class="c-footerBootom__icon">
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "FooterBottom",
+    data: function() {
+        return {
+            iconLinks: [
+                {
+                    text: "DC Facebook",
+                    url: "#",
+                    img: "footer-facebook.png",
+                },
+                {
+                    text: "DC Twitter",
+                    url: "#",
+                    img: "footer-twitter.png",
+                },
+                {
+                    text: "DC Youtube",
+                    url: "#",
+                    img: "footer-youtube.png",
+                },
+                {
+                    text: "DC Pinterest",
+                    url: "#",
+                    img: "footer-pinterest.png",
+                },
+                {
+                    text: "DC Periscope",
+                    url: "#",
+                    img: "footer-periscope.png",
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../assets/style/variables.scss";
+@import "../assets/style/common.scss";
+
+.c-footerBottom {
+    background-color: $gray-600;
+    padding: 1rem;
+
+}
+</style>
